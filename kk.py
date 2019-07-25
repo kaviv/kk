351aefb17d4340d0b2bfde69ca798b793cf1274c
kaviv <53109444+kaviv@users.noreply.github.com>
2019-07-25 17:47:15 +0530

    Create kk.py

---
+++ kk.py
@@ -0,0 +1,7 @@
+char = input("Please Enter Your Own Character : ")
+ 
+if(char== 'a' or char== 'e' or char== 'i' or char== 'o' or char== 'u' or char == 'A'
+       or char== 'E' or char== 'I' or char == 'O' or char == 'U'):
+    print("Valid")
+else:
+    print("Invalid")
